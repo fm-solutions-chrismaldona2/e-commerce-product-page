133349016db06ce662599a99fed42b9186e1dbff
Christian Maldonado <chris.ariel.maldonado@gmail.com>
2025-01-15 13:38:31 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "@src/styles/reset.css";
+import "@src/styles/fonts.css";
+import "@src/styles/global.css";
+
+function App() {
+  return <></>;
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@src": "/src/",
+      "@shared": "/src/shared/",
+      "@pages": "/src/pages/",
+      "@assets": "/src/assets/",
+      "@features": "/src/features/",
+    },
+  },
+});
